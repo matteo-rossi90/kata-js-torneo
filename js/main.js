0a3a0cb8c3fc4ce4883a975a54569ca8bfdf233e
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-11-26 17:04:26 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,101 @@
+const fighters = [
+    {
+        name: 'Freezer',
+        power: 8000
+    },
+    {
+        name: 'Vegeta',
+        power: 8500
+    },
+    {
+        name: 'Crilin',
+        power: 500
+    },
+    {
+        name: 'Mr Satan',
+        power: 50
+    },
+    {
+        name: 'Junior',
+        power: 6000
+    },
+    {
+        name: 'Goku',
+        power: 9001
+    },
+    {
+        name: 'Tensing',
+        power: 450
+    },
+    {
+        name: 'Videl',
+        power: 300
+    },
+    {
+        name: 'Bulma',
+        power: 20
+    },
+    {
+        name: 'C-18',
+        power: 7800
+    },
+    {
+        name: 'Gohan',
+        power: 8900
+    },
+    {
+        name: 'Trunks',
+        power: 1250
+    }
+];
+
+const weapons = [
+    {
+        name: "Ventaglio della Musa",
+        power: 15
+    },
+    {
+        name: "Scouter",
+        power: 30
+    },
+    {
+        name: "Bastone Roshi",
+        power: 60
+    },
+    {
+        name: "Fagioli Magici",
+        power: 70
+    },
+    {
+        name: "Katana di Yajirobei",
+        power: 85
+    },
+    {
+        name: "Spada del Dragone Azzurro",
+        power: 115
+    },
+    {
+        name: "Armatura Saiyan",
+        power: 145
+    },
+    {
+        name: "Cannone da braccio",
+        power: 170
+    },
+    {
+        name: "Nuvola d'oro",
+        power: 200
+    },
+    {
+        name: "Bastone Nyoi",
+        power: 220
+    },
+    {
+        name: "Spada Z",
+        power: 235
+    },
+    {
+        name: "Orecchini Potara",
+        power: 250
+    }
+];
